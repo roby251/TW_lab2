8eee17be15eadb2980aaddc3334d889ff09e9c1b
JackDTowers <113446734+JackDTowers@users.noreply.github.com>
2022-10-16 17:26:06 -0300

    .

--- js/cargajson.js
+++ js/cargajson.js
@@ -1,3 +1,21 @@
 function main(){
-    var value = document.getElementById("")
+    var value = document.getElementById("MC1").value;
+
+    switch (value) {
+        case "1":
+            url = "chile.json";
+          break;
+        case "2":
+            url = "espana.json";
+          break;
+        case "3":
+            url = "argentina.json";
+          break;
+        case "4":
+            url = "mexico.json";
+          break;
+        default:
+          console.log("Nada");
+          break;
+      }
 }
